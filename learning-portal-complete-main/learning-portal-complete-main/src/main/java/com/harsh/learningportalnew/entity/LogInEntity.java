a34478bd91a74510b56b869444eb71406d2b8e32
132905 <125987008+132905@users.noreply.github.com>
2024-02-20 22:22:44 +0530

    Created LogInEntity

---
+++ learning-portal-complete-main/learning-portal-complete-main/src/main/java/com/harsh/learningportalnew/entity/LogInEntity.java
@@ -0,0 +1,13 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class LogInEntity {
+	
+	private String  username;
+	private String password;
+
+}
